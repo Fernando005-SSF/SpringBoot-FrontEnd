d957b7e22a918458b9e69c6c1c20157d040e3f4d
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-03-04 13:42:53 +0530

    Created the homapage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
+  
     </div>
   );
 }
