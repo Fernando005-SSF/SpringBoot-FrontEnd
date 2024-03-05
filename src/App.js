4a4827f95d12add290db0b805a2c474e82242373
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-03-05 11:44:18 +0530

    Cfreated the registration form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import AddUser from './Users/AddUser';
 
 
 function App() {
   return (
     <div>
+
+      <Router>
       <Navbar/>
-      <Home/>
+
+      <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/AddUser' element={<AddUser/>}/>
+      </Routes>
+
+      </Router>
   
     </div>
   );
--- src/Users/AddUser.jsx
+++ src/Users/AddUser.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const AddUser = () => {
   return (
-    <div>
+    <div className="container">
+
+      <div className="row">
+
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-5 shadow">
+          <h2 className="text-center m-4" >Register Form</h2>
+
+          <div className="mb-3">
+
+            <label htmlFor="Name" className="form-label">Name</label>
+            <input type="text" className="form-control" placeholder='Enter your name' name='name' />
+
+          </div>
+
+          <div className="mb-3">
+
+            <label htmlFor="UserName" className="form-label">User Name</label>
+            <input type="text" className="form-control" placeholder='Enter your user name' name='UserName' />
+
+          </div>
+
+          <div className="mb-3">
+
+            <label htmlFor="Email" className="form-label">Email</label>
+            <input type="text" className="form-control" placeholder='Enter your Email' name='Email' />
+
+          </div>
+
+          <button type='submit' className="btn btn-outline-primary">Submit</button>
+          <button type='submit' className="btn btn-outline-danger m-2 ">Cancel</button>
+
+        
+        </div>
+      </div>
       
     </div>
   )
